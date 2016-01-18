a323cdb616a5f39af9e1ad92d7c5ac7e643c0696
wangxiaoming <wangxiaoming@shopin.cn>
2016-01-18 15:11:48 +0800

    inti commit

---
+++ src/net/shopin/git/Hello.java
@@ -0,0 +1,26 @@
+/**
+ * Hello.java
+ * net.shopin.git
+ *
+ * Function： TODO 
+ *
+ *   ver     date      		author
+ * ──────────────────────────────────
+ *   1.0   2016年1月18日  	 wangxiaoming
+ *
+ * Copyright (c) 2016, TNT All Rights Reserved.
+*/
+
+package net.shopin.git;
+/**
+ * <p>ClassName:Hello</p>
+ * <p>Description:	</p>
+ * <p>Company: www.shopin.net</p>
+ * @author   wangxiaoming
+ * @version  1.0
+ * @Date	 2016年1月18日下午2:56:49
+ */
+public class Hello {
+
+}
+
